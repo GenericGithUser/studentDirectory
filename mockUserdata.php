51da80376e18abbe05f06abf3847f84e9b1e43fb
GenericGithUser <blgold567@gmail.com>
2023-06-10 13:48:13 +0800

    Made Userdata actually show userdata, also made dummy form for modify button

--- mockUserdata.php
+++ mockUserdata.php
@@ -1,3 +1,33 @@
+<?php
+session_start();
+// Include Connection Function
+include 'credentials.php';
+// Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to login.php
+    header("Location: login.php");
+    exit;
+  }
+  try{
+    $pdo = connect();
+    // Set the PDO error mode to exception
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $LRN = null; 
+        if(isset($_GET['LRN'])){
+            $LRN = $_GET['LRN'];
+            $viewDetails = "SELECT * FROM tblstudents WHERE LRN= :LRN";
+            $stmt = $pdo->prepare($viewDetails);
+            $stmt->bindParam(':LRN', $LRN);
+            $stmt->execute();
+            $select = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        
+        }catch(PDOException $e) {
+    // Display an error message if unable to connect to the database
+    echo "Connection failed: " . $e->getMessage();
+  }
+  $pdo = null;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,25 +54,90 @@
             </div>
         </div>
         <div class="contents">
+        <dialog class="edit" >
+                 <h2>Edit Details</h2>
+                 <form action="" method="POST" class="edit-form">
+                    <div class="FCont">
+                    <label for="LRN">Learner's Reference Number</label>
+                    <input type="number" id="LRN" name="LRN" class="aSB_inpbx" maxlength="13">
+                </div>
+                <div class="FCont">
+                    <label for="fname">First Name</label>
+                    <input type="text" id="fname" name="fname" class="aSB_inpbx" required>
+                </div>
+                <div class="FCont">
+                    <label for="mname">Middle Name</label>
+                    <input type="text" id="mname" name="mname" class="aSB_inpbx" maxlength="10" required>
+                </div>
+                <div class="FCont">
+                    <label for="lname">Last Name</label>
+                    <input type="text" id="lname" name="lname" class="aSB_inpbx" required>
+                </div>
+                <div class="FCont">
+                    <label for="age">Age</label>
+                    <input type="number" id="age" name="age" class="aSB_inpbx" style="width:80px" maxlength="10" min="1" required>
+                    <label for="gender" style="width:100px">Gender</label>
+                    <select name="gender" id="gender" class="selectOption" required>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div class="FCont">
+                    <label for="Birthday">Date of Birth</label>
+                    <input type="date" name="Birthday" id="Birthday" required>
+                </div>
+                <div class="FCont">
+                    <label for="GrdLvl" style="width:260px">Grade to be enrolled</label>
+                    <select name="GrdLvl" id="GrdLvl" class="selectOption" required>
+                            <option value="none">Select A Grade</option>
+                            <option value="7">Grade 7</option>
+                            <option value="8">Grade 8</option>
+                            <option value="9">Grade 9</option>
+                            <option value="10">Grade 10</option>
+                            <option value="11">Grade 11</option>
+                            <option value="12">Grade 12</option>
+                        </select>
+                </div>
+                <div class="FCont">
+                    <label for="strand">Strand</label>
+                    <select name="strand" id="strand" class="selectOption" required>
+                        <option value="STEM">STEM</option>
+                        <option value="ABM">ABM</option>
+                        <option value="GAS">GAS</option>
+                        <option value="TVL">TVL</option>
+                    </select>
+                    <label for="pNumber">Phone Number</label>
+                    <input type="number" name="pNumber" id="pNumber" style="width:200px" maxlength="12" class="aSB_inpbx">
+                </div>
+                <div class="FCont">
+                    <button onclick="closeEdit()" class="aSB_btn">Submit</button>
+                </div>
+                 </form>
+                </dialog>
             <div class="genInfo">
                 <div class="head">
                     <h1>General Information</h1>
                         <div class="opt">
-                            <div class="modifyBtn btn">Modify</div>
+                            <div class="modifyBtn btn" onclick="showEdit()">Modify</div>
                             <div class="deleteBtn btn">DELETE</div>
                         </div>
                 </div>
+                <?php foreach($select as $result){?>
                 <div class="info">
-                    <h2>Learner's Reference Number: <span class="name">00000000000</span></h2>
-                    <h2>Name:<span class="name"> John Doe</span></h2>
-                    <h2>Age:<span class="name"> 00</span></h2>
-                    <h2>Gender:<span class="name">Gender</span></h2>
-                    <h2>Birthday:<span class="name">Month-Day-Year</span></h2>
-                    <h2>Grade Level:<span class="name">00</span></h2>
-                    <h2>Strand:<span class="name">XXX</span></h2>
-                    <h2>Date Enrolled:<span class="name">Month-Day-Year</span></h2>
-                    <h2>Phone Number:<span class="name">0000000000000</span></h2>
+                    <h2>Learner's Reference Number: <span class="name"><?php echo $result['LRN']; ?></span></h2>
+                    <h2>Name:<span class="name"><?php echo $result['FirstName']. " ".$result['MiddleIntial']. " ".$result['LastName']; ?></span></h2>
+                    <h2>Age:<span class="name"><?php echo $result['Age'];?></span></h2>
+                    <h2>Gender:<span class="name"><?php echo $result['Gender'];?></span></h2>
+                    <h2>Birthday:<span class="name"><?php echo date('F d,Y',strtotime($result['Birthday']));?></span></h2>
+                    <h2>Grade Level:<span class="name"><?php echo $result['GradeLevel'];?></span></h2>
+                    <h2>Strand:<span class="name"><?php echo $result['Strand'];?></span></h2>
+                    <h2>Date Enrolled:<span class="name"><?php echo date('F d,Y',strtotime($result['DateEnrolled']));?></span></h2>
+                    <h2>Phone Number:<span class="name"><?php echo $result['PhoneNumber'];?></span></h2>
+                    
                 </div>
+                <?php }?>
+                
             </div>
             <div class="grades">
                 <div class="head">
@@ -147,6 +242,7 @@
                     </div>
                 </div>
             </div>
+            
         </div>
     </div>
 </body>
@@ -155,6 +251,7 @@
         const grades = document.querySelector('.opt2');
         const genInfoDis = document.querySelector('.genInfo');
         const gradesDis = document.querySelector('.grades');
+        const editPop = document.querySelector('.edit');
         function switcheri(){
             genInfo.classList.remove("active");
             grades.classList.add("active");
@@ -167,6 +264,11 @@
             genInfoDis.style.display="block";
             gradesDis.style.display="none";
         }
-    
+        function showEdit(){
+            editPop.showModal();
+        }
+        function closeEdit(){
+            editPop.close();
+        }
 </script>
 </html>
--- page.php
+++ page.php
@@ -87,7 +87,7 @@ if (!isset($_SESSION['user_id'])) {
         <div class="AllRecords">
             <h2>List of all students</h2>
             <div class="list">
-            < <?php 
+             <?php 
              try{
                 $pdo = connect();
                 // Set the PDO error mode to exception
@@ -96,10 +96,10 @@ if (!isset($_SESSION['user_id'])) {
                  $stmt = $pdo->prepare($viewAllSQL);
                  $stmt->execute();
                  $viewAll = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+                //show all results
                  if(count($viewAll)>0){
                     foreach($viewAll as $result){
-                        echo '<a href="mockUserdata.php"><div class="result">'.$result['FirstName'].' '. $result['LastName'].'</div></a>';
+                        echo "<a href='mockUserdata.php?LRN=".$result['LRN']."'><div class='result'>".$result['FirstName']." ".$result['LastName']."</div></a>";
                     }
                  }
 
@@ -115,7 +115,7 @@ if (!isset($_SESSION['user_id'])) {
         <!--Sorry for long form and a lot of divs-->
         <div class="addStuBox">
             <h2>Add a Student</h2>
-            <form action="" class="aSB_form">
+            <form action="page.php" class="aSB_form" method="POST">
             <div class="FCont">
                     <label for="LRN" style="width: 300px; margin-left: -150px;">Learner's Reference Number</label>
                     <input type="number" id="LRN" name="LRN" class="aSB_inpbx" maxlength="13">
@@ -177,51 +177,53 @@ if (!isset($_SESSION['user_id'])) {
             <?php
             //Apparently Making it inline makes the inputting process work
             //This just made the code longer i guess?
-                try {
-                    $pdo = connect();
-                    // Set the PDO error mode to exception
-                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                  
-                      if($_SERVER['REQUEST_METHOD'] === "POST"){
-                          $LRN = $_POST['LRN'];
-                          $fname = $_POST['fname'];
-                          $mname = substr($_POST['mname'],0,1);
-                          $lname = $_POST['lname'];
-                          $age = $_POST['age'];
-                          $gender = $_POST['gender'];
-                          $Birthday = $_POST['Birthday'];
-                          $GrdLvl = $_POST['GrdLvl'];
-                          $strand = $_POST['strand'];
-                          $denrolld = date("Y-m-d");
-                          $pNumber = $_POST['pNumber'];
-                          
-                          $sql = "INSERT INTO tblstudents (LRN, FirstName, MiddleIntial, LastName, Age, Gender, Birthday, GradeLevel, Strand, DateEnrolled, PhoneNumber)
-                          VALUES(:LRN, :fname, :mname, :lname, :age, :gender, :Birthday, :grdlvl, :strand, :denrolld, :pNumber)";
-                          $stmt = $pdo->prepare($sql);
-                          $stmt->bindParam(':LRN',$LRN);
-                          $stmt->bindParam(':fname',$fname);
-                          $stmt->bindParam(':mname',$mname);
-                          $stmt->bindParam(':lname',$lname);
-                          $stmt->bindParam(':age',$age);
-                          $stmt->bindParam(':gender',$gender);
-                          $stmt->bindParam(':Birthday',$Birthday);
-                          $stmt->bindParam(':grdlvl',$GrdLvl);
-                          $stmt->bindParam(':strand',$strand);
-                          $stmt->bindParam(':denrolld',$denrolld);
-                          $stmt->bindParam(':pNumber',$pNumber);
-                          if ($stmt->execute()) {
-                            echo "Executed";
-                          } else {
-                            $errorInfo = $stmt->errorInfo();
-                            echo "Error: " . $errorInfo[2];
-                          }
+            try {
+                $pdo = connect();
+                // Set the PDO error mode to exception
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                  if($_SERVER['REQUEST_METHOD'] === "POST"){
+                    //get the varibles
+                      $LRN = $_POST['LRN'];
+                      $fname = $_POST['fname'];
+                      $mname = substr($_POST['mname'],0,1);
+                      $lname = $_POST['lname'];
+                      $age = $_POST['age'];
+                      $gender = $_POST['gender'];
+                      $Birthday = $_POST['Birthday'];
+                      $GrdLvl = $_POST['GrdLvl'];
+                      $strand = $_POST['strand'];
+                      $denrolld = date("Y-m-d");
+                      $pNumber = $_POST['pNumber'];
+                      //SQL Code and Execution
+                      $sql = "INSERT INTO tblstudents (LRN, FirstName, MiddleIntial, LastName, Age, Gender, Birthday, GradeLevel, Strand, DateEnrolled, PhoneNumber)
+                      VALUES(:LRN, :fname, :mname, :lname, :age, :gender, :Birthday, :grdlvl, :strand, :denrolld, :pNumber)";
+                      $stmt = $pdo->prepare($sql);
+                      $stmt->bindParam(':LRN',$LRN);
+                      $stmt->bindParam(':fname',$fname);
+                      $stmt->bindParam(':mname',$mname);
+                      $stmt->bindParam(':lname',$lname);
+                      $stmt->bindParam(':age',$age);
+                      $stmt->bindParam(':gender',$gender);
+                      $stmt->bindParam(':Birthday',$Birthday);
+                      $stmt->bindParam(':grdlvl',$GrdLvl);
+                      $stmt->bindParam(':strand',$strand);
+                      $stmt->bindParam(':denrolld',$denrolld);
+                      $stmt->bindParam(':pNumber',$pNumber);
+                      if ($stmt->execute()) {
+                        
+                      } else {
+                        $errorInfo = $stmt->errorInfo();
+                        echo "Error: " . $errorInfo[2];
                       }
-                  
-                  }catch (PDOException $e) {
-                    // Display an error message if unable to connect to the database
-                    echo "Connection failed: " . $e->getMessage();
                   }
-                  
+              
+              }catch (PDOException $e) {
+                // Display an error message if unable to connect to the database
+                echo "Connection failed: " . $e->getMessage();
+              }
+              $pdo = null; 
+      
             ?>
         </div>
     </div>
@@ -265,6 +267,5 @@ if (!isset($_SESSION['user_id'])) {
         resultBox.style.display = "block";
     }
   
-
 </script>
 </html>
