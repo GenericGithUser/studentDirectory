18803708d1b54cd95ba0d1afdadc958e686c31bd
tsk <juliussan123@gmail.com>
2023-06-11 10:30:45 +0800

    Omitted the redundant dashboard file and fixed the credentials.php implementation

--- create.php
+++ create.php
@@ -1,42 +1,25 @@
 <?php
-// Include Server Connection Function
-include 'credentials.php';
 
-// Create a new PDO instance
-try {
-  $pdo = connect();
-  // Set the PDO error mode to exception
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+function connect(){
+  // Database credentials
+  $hostname = 'localhost';
+  $username = 'root';
+  $password = '';
+  $database = 'dbstudentdirectory';
 
-  // Check if the form has been submitted
-  if(isset($_POST['submit'])) {
-    // Sanitize data from form
+  try {
+    // Create a new PDO instance
+    $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
 
-    $name = htmlspecialchars($_POST["name"], ENT_QUOTES);
-    $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-    $password = htmlspecialchars($_POST["password"], ENT_QUOTES);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-    // Hash the password before storing it in the database
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    // Execute SQL query to insert data into database
-    $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':name', $name);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':password', $hashed_password);
-    $stmt->execute();
-
-    // Redirect to index.php with success message
-    header("Location: index.php?success=true");
-    exit;
+    return $pdo;
+  } catch (PDOException $e) {
+    // Display an error message if unable to connect to the database
+    echo "Connection failed: " . $e->getMessage();
+    die();
   }
-} catch(PDOException $e) {
-  // Display an error message if unable to connect to the database
-  echo "Connection failed: " . $e->getMessage();
 }
 
-// Close the database connection
-$pdo = null;
 ?>
--- login.php
+++ login.php
@@ -1,77 +1,63 @@
-  <?php
-  session_start();
+<?php
+session_start();
+
 // Include Server Connection Function
 include 'credentials.php';
-  
 
-  // Create a new PDO instance
-  try {
-    $pdo = connect();
-    // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Create a new PDO instance
+try {
+  $pdo = connect();
+  // Set the PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Check if the form has been submitted
-    if(isset($_POST['submit'])) {
-      // Retrieve data from form
-      $email = $_POST["email"];
-      $password = $_POST["password"];
+  // Check if the form has been submitted
+  if(isset($_POST['submit'])) {
+    // Retrieve data from form
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-      // Execute SQL query to retrieve data from database
-      $sql = "SELECT * FROM users WHERE email = :email";
-      $stmt = $pdo->prepare($sql);
-      $stmt->bindParam(':email', $email);
-      $stmt->execute();
-      $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Execute SQL query to retrieve data from database
+    $sql = "SELECT * FROM users WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-      // Verify password
-      if ($user && password_verify($password, $user['password'])) {
-        // Store user information in session
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['user_email'] = $user['email'];
-        $_SESSION['user_name'] = $user['name'];
+    // Verify password
+    if ($user && password_verify($password, $user['password'])) {
+      // Store user information in session
+      $_SESSION['user_id'] = $user['id'];
+      $_SESSION['user_email'] = $user['email'];
+      $_SESSION['user_name'] = $user['name'];
 
-        // Redirect to dashboard.php
-        header("Location: dashboard.php");
-        exit;
-      } else {
-        // Redirect to error.php
-        header("Location: error.php");
-        exit;
-      }
+      // Redirect to dashboard.php
+      header("Location: dashboard.php");
+      exit;
+    } else {
+      // Redirect to error.php
+      header("Location: error.php");
+      exit;
     }
-  } catch(PDOException $e) {
-    // Display an error message if unable to connect to the database
-    echo "Connection failed: " . $e->getMessage();
   }
+} catch(PDOException $e) {
+  // Display an error message if unable to connect to the database
+  echo "Connection failed: " . $e->getMessage();
+}
 
-  // Close the database connection
-  $pdo = null;
-  ?>
+// Close the database connection
+$pdo = null;
+?>
 
-
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Login</title>
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <form action="login.php" method="POST">
-    <h1>SFHS Student Directory</h1>
-      <div class="internalFormWrapper">
-      <p>
-        <label for="email"></label>
-        <input type="email" id="email" name="email" required placeholder="Email">
-      </p>
-      <p>
-        <label for="password"></label>
-        <input type="password" id="password" name="password" required placeholder="password">
-      </p>
-  </div>
-      <p class="chooseButtonWrapper">
-        <button type="submit" name="submit">Login</button>
-        <a href="register.php"><button  type="button"  >register</button></a>
-      </p>
-    </form>
-  </body>
-  </html>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <form action="login.php" method="POST">
+  <h1>SFHS Student Directory</h1>
+    <div class="internalFormWrapper">
+    <p>
+      <label for="email"></label>
+      <input type="email" id
