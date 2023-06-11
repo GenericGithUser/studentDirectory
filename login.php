1a51325acf9eba6e0181820d0f11c4cd5336c646
Tsk <96981837+2Tsk2@users.noreply.github.com>
2023-06-11 11:20:00 +0800

    Updated login.php as i think it broke after the force push

--- login.php
+++ login.php
@@ -16,58 +16,52 @@ try {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    // Execute SQL query to retrieve data from database
+    // Execute SQL query to retrieve data from the first table
     $sql = "SELECT * FROM users WHERE email = :email";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':email', $email);
     $stmt->execute();
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-      // Check table if user exists
-      if($user){
-        if ($user && password_verify($password, $user['password'])) {
-          // Store user information in session
-          $_SESSION['user_id'] = $user['id'];
-          $_SESSION['user_email'] = $user['email'];
-          $_SESSION['user_name'] = $user['name'];
-  
-          // Redirect to dashboard.php
-          header("Location: dashboard.php");
-          exit;
-        } else {
-          // Redirect to error.php
-          header("Location: error.php");
-          exit;
-        }
-        //Check second table if first does not show results
-      } else{
-        $sql = "SELECT * FROM tblstudents WHERE email = :email";
+    // Check if user exists in the first table
+    if($user) {
+      if ($user && password_verify($password, $user['password'])) {
+        // Store user information in session
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['user_email'] = $user['email'];
+        $_SESSION['user_name'] = $user['name'];
+
+        // Redirect to dashboard.php
+        header("Location: dashboard.php");
+        exit;
+      }
+    } else {
+      // Execute SQL query to retrieve data from the second table
+      $sql = "SELECT * FROM tblstudents WHERE email = :email";
       $stmt = $pdo->prepare($sql);
       $stmt->bindParam(':email', $email);
       $stmt->execute();
       $user = $stmt->fetch(PDO::FETCH_ASSOC);
-      if($user){
-        if ($user && password_verify($password, $user['password'])) {
-          // Store user information in session
-          $_SESSION['user_id'] = $user['LRN'];
-          $_SESSION['user_email'] = $user['email'];
-          $_SESSION['user_name'] = $user['FirstName'];
-          $_SESSION['student'] = "true";
-  
-          // Redirect to dashboard.php
-          header("Location: stuPage.php");
-          exit;
-        } else {
-          // Redirect to error.php
-          header("Location: error.php");
-          exit;
-        }
+
+      // Check if user exists in the second table
+      if ($user && password_verify($password, $user['password'])) {
+        // Store user information in session
+        $_SESSION['user_id'] = $user['LRN'];
+        $_SESSION['user_email'] = $user['email'];
+        $_SESSION['user_name'] = $user['FirstName'];
+        $_SESSION['student'] = "true";
+
+        // Redirect to stuPage.php
+        header("Location: stuPage.php");
+        exit;
       }
-      }
-     
     }
+
+    // Redirect to error.php if login fails
+    header("Location: error.php");
+    exit;
   }
- catch(PDOException $e) {
+} catch(PDOException $e) {
   // Display an error message if unable to connect to the database
   echo "Connection failed: " . $e->getMessage();
 }
@@ -84,8 +78,21 @@ $pdo = null;
 </head>
 <body>
   <form action="login.php" method="POST">
-  <h1>SFHS Student Directory</h1>
+    <h1>SFHS Student Directory</h1>
     <div class="internalFormWrapper">
-    <p>
-      <label for="email"></label>
-      <input type="email" id
+      <p>
+        <label for="email"></label>
+        <input type="email" id="email" name="email" required placeholder="Email">
+      </p>
+      <p>
+        <label for="password"></label>
+        <input type="password" id="password" name="password" required placeholder="Password">
+      </p>
+    </div>
+    <p class="chooseButtonWrapper">
+      <button type="submit" name="submit">Login</button>
+      <a href="register.php"><button type="button">Register</button></a>
+    </p>
+  </form>
+</body>
+</html>
